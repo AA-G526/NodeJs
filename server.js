3b9a2e8cd0e2d2082cc204b487e28ab93801a280
abriel <abriel.devs@gmail.com>
2025-04-03 15:41:17 +0700

    make routes and handling req/res

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const server = require("express");
+const path = require("path");
 const app = server();
 const port = 5959;
 
@@ -6,6 +7,9 @@ app.get('/',(req,res) => {
     res.send("hello world");
 });
 
+app.get('/admin', (req,res) => {
+    res.sendFile(path.join(__dirname ,'index.html'));
+})
 app.listen(port, () => {
     console.log(`Server is runnin at port : ${port}!`);
 })
