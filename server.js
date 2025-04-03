d9facc3a131a3d0957e7b1d5ab55bee9ce5f278f
abriel <abriel.devs@gmail.com>
2025-04-03 16:32:28 +0700

    initial commit to build a project

--- server.js
+++ server.js
@@ -4,29 +4,8 @@ const app = server();
 const port = 5959;
 
 app.get('/',(req,res) => {
-    res.send("hello world");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
-
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname ,'index.html'));
-});
-
-app.get('/search', (req, res) => {
-  const query = req.query.q || 'No search term provided';
-  const page = req.query.page || 1;
-  const filter = req.query.f || 'No filter';
-  if (query == "product") {
-    res.send(`Products...`);
-  } else if (query == "dashboard") {
-    res.send("dahsboard");
-  } else if (filter == "product") {
-    res.send("you filtered product");
-  }
-  else {
-    res.send(`You searched for: ${query}. Page: ${page} and filtering : ${filter}`);
-    }
-});
-
 
 app.listen(port, () => {
     console.log(`Server is runnin at port : ${port}!`);
